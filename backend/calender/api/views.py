1256ccf6717b542251a302722a3931b2e0a68306
NishPatelCodes <Nishpatel20202@gmail.com>
2026-04-11 01:13:44 -0300

    Integrated Gemini- crafted JSON from AI

--- backend/calender/api/views.py
+++ backend/calender/api/views.py
@@ -1,26 +1,64 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+from google import genai
+from django.http import JsonResponse
+import json
+from google import genai
+
 
 def hello(request):
     return render(request,"calendar.html")
 
-from django.http import JsonResponse
-import json
 
 @csrf_exempt
 def add_event(request):
     if request.method == "POST":
         try:
             data = json.loads(request.body)   # parse JSON
-            text = data.get("text")           # extract text
+            user_input = data.get("user_input")           # extract user_input
 
-            print("User said:", text)
+            #AI integration
+
+            #gemini client checks API key in your env variables
+            client = genai.Client()
+
+            #pass model and query
+            response = client.models.generate_content(
+                model="gemini-3-flash-preview", 
+                contents = f"""
+                    Convert the following input into JSON.
+                    User input: {user_input}
+                    Return ONLY valid JSON in this format:
+                    {{
+                    "action": "add_event",
+                    "title": "...",
+                    "date": "YYYY-MM-DD"
+                    }}
+                    """)
+            
+            #Get the valid JSON from AI 
+            print(response.text)
+
+
+
+
+
+            #PENDING
+            #return response.txt to frontend
+            #In frontend, make logical function to execute json to action
+            #Pass that json response to the function
+            #Test
+
+
+
+
+            print("User said:", user_input)
 
             # For now, just return it back
             return JsonResponse({
                 "status": "success",
-                "received_text": text
+                "received_user_input": user_input
             })
 
         except Exception as e:
@@ -30,4 +68,3 @@ def add_event(request):
             }, status=400)
 
     return JsonResponse({"error": "Only POST allowed"}, status=405)
-
